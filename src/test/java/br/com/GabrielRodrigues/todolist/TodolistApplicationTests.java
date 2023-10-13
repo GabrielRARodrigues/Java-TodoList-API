ec6423506ca2421b4e9d48d4d3bc3d4cd2f1dfdf
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-10-13 20:40:44 -0300

    Starting a REST Todolist application with Java and Spring
    Starting the development of a Java application using the Spring Framework to create a 'to-do list' application. Initiating the application by creating a 'user' package, along with the 'UserController' class representing the user route controller, and the 'UserModel' class representing the user model

---
+++ src/test/java/br/com/GabrielRodrigues/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GabrielRodrigues.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
